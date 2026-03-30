89933d83f251d715061253b45331a1e26f982a65
Tharusi Manchanayake <tharusimanchanayake45@gmail.com>
2026-03-30 21:07:57 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
+
 const { Router } = require("express");
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 const newsRoutes = require("./news.routes");
+const busRoutes = require("./bus.routes");
+const routeRoutes = require("./route.routes");
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/news", newsRoutes);
+router.use("/buses", busRoutes);
+router.use("/routes", routeRoutes);
 
 module.exports = router;
